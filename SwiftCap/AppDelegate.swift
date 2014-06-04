4f3fec42d7469c51c8b506b824486eedb38e8690
Kendrick Ledet <kendrickledet@gmail.com>
2014-06-04 11:48:59 -0500

    Initial Commit

---
+++ SwiftCap/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftCap
+//
+//  Created by kenny on 6/4/14.
+//  Copyright (c) 2014 Kendrick Ledet. All rights reserved.
+//
+
+import Cocoa
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+                            
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification?) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification?) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ SwiftCap/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  SwiftCap
+//
+//  Created by kenny on 6/4/14.
+//  Copyright (c) 2014 Kendrick Ledet. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+                                    
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+                                    
+    }
+
+
+}
+
